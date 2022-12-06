5067f88291ff83ea6720e5c538c38525a8ccc723
wgorick <wgorick@yext.com>
2022-12-06 07:41:27 -0700

    day 6

---
+++ day6/part1.py
@@ -0,0 +1,39 @@
+import sys
+import os.path
+
+REPO = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(REPO)
+sys.path.append(f'{REPO}/util')
+from collections import deque
+
+from solution import Solution
+
+TEST_CASES = [
+{
+"input": """mjqjpqmgbljsphdztnvjfqwrcgsmlb""",
+"solution": 7
+}
+]
+  
+def main():
+  solution = PartSolution(__file__, sys.argv)
+  solution.test_cases = TEST_CASES
+  solution.run()
+  
+class PartSolution(Solution):
+  def solve(self, inp, test=False):
+    inp_str = inp[0]
+    current = deque()
+    for i in range(len(inp_str)):
+      current.append(inp_str[i])
+      if len(current) > 3:
+        curr_set = set(current)
+        if len(curr_set) == 4:
+          return i +1
+        current.popleft()
+    res = 0
+    return res
+    
+
+if __name__ == "__main__":
+  main()
