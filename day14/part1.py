68da096256343e082928c1154c01d24cf9941c66
wgorick <wgorick@yext.com>
2022-12-14 01:04:35 -0500

    day 14

---
+++ day14/part1.py
@@ -0,0 +1,93 @@
+import sys
+import os.path
+
+REPO = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(REPO)
+sys.path.append(f'{REPO}/util')
+
+from solution import Solution
+
+TEST_CASES = [
+{
+"input": """498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9""",
+"solution": 24
+}
+]
+  
+def main():
+  solution = PartSolution(__file__, sys.argv)
+  solution.test_cases = TEST_CASES
+  solution.run()
+  
+class PartSolution(Solution):
+  def solve(self, inp, test=False):
+    self.res = 0
+    self.grid = [['.' for _ in range(200)] for _ in range(200)]
+    self.max_depth = -1
+    for line in inp:
+      coords = line.split(" -> ")
+      start_x, start_y = -1, -1
+      for coord in coords:
+        x, y = coord.split(",")
+        if start_x != -1:
+          self.draw(start_x, x, start_y, y)
+        start_x = x
+        start_y = y
+    
+    while(self.can_drop()):
+      self.res += 1
+    return self.res
+  
+  def can_drop(self):
+    x, y = 100, 0
+    can_drop = True
+
+    while can_drop:
+      if y > self.max_depth:
+        return False
+      if self.grid[y+1][x] != "#":
+        y += 1
+      elif self.grid[y+1][x-1] != "#":
+        y += 1
+        x -= 1
+      elif self.grid[y+1][x+1] != "#":
+        y += 1
+        x += 1
+      else:
+        can_drop == False
+        break
+    self.grid[y][x] = "#"
+    return True
+
+  def draw(self, start_x, x, start_y, y):
+    start_x = int(start_x)-400
+    x = int(x)-400
+    start_y = int(start_y)
+    y = int(y)
+
+    if y > self.max_depth:
+      self.max_depth = y
+
+    if start_y > self.max_depth:
+      self.max_depth = start_y
+
+    while start_x < x:
+      self.grid[start_y][start_x] = "#"
+      start_x += 1
+
+    while start_x > x:
+      self.grid[start_y][start_x] = "#"
+      start_x -= 1
+
+    while start_y < y:
+      self.grid[start_y][start_x] = "#"
+      start_y += 1
+
+    while start_y > y:
+      self.grid[start_y][start_x] = "#"
+      start_y -= 1
+
+    self.grid[y][x] = "#"
+if __name__ == "__main__":
+  main()
