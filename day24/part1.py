6980028111ddabed19dda0df855ab6d8978bc5a6
wgorick <wgorick@yext.com>
2022-12-24 16:06:48 -0500

    much faster bfs

--- day24/part1.py
+++ day24/part1.py
@@ -11,18 +11,23 @@ sys.path.append(f'{REPO}/util')
 from solution import Solution
 
 TEST_CASES = [
-{
-"input": 
-"""#.######
-#>>.<^<#
-#.<..<<#
-#>v.><>#
-#<^v^^>#
-######.#""",
-"solution": 18
-}
 ]
-  
+
+class Wind:
+  def __init__(self, x, y, deltas, width, height):
+    self.x = x
+    self.y = y
+    self.dx = deltas[0]
+    self.dy = deltas[1]
+    self.width = width
+    self.height = height
+  def __repr__(self):
+    return f"{self.x}, {self.y}, {self.dx}, {self.dy}"
+      
+  def move(self):
+    self.x=(self.x+self.dx)%self.width
+    self.y=(self.y+self.dy)%self.height
+
 def main():
   solution = PartSolution(__file__, sys.argv)
   solution.test_cases = TEST_CASES
@@ -30,128 +35,47 @@ def main():
   
 class PartSolution(Solution):
   def solve(self, inp, test=False):
-    DEBUG = "debug" in sys.argv
+    lines = [line for line in inp]
+    deltas_from_char = {'v': (0,1), '^': (0,-1), '>': (1,0), '<': (-1,0)}
 
-    #initialize self.res to inf, then decrease as we find better solutions
-    self.res = float("inf")
-    
-    self.dirs = [(1,0), (0,1), (0,-1), (-1, 0)]
+    self.width = len(lines[0]) - 2
+    self.height = len(lines) - 2
+    self.dirs = [(1,0), (0,1), (0,-1), (-1, 0), (0, 0)]
 
-    #initial layout of the field
-    initial_field = defaultdict(set)
+    start = (0, -1)
+    end = (self.width-1, self.height)
 
-    #start coordinate
-    start = (0, 1)
+    self.winds = [Wind(x, y, deltas_from_char[char], self.width, self.height) for y, line in enumerate(lines[1:-1]) for x, char in enumerate(line[1:-1]) if char != '.']
 
-    #end coordinate
-    self.goal = (len(inp)-1, len(inp[0])-2)
-    print(f"Goal: {self.goal}")
-    #height and width of the field
-    self.height, self.width = len(inp), len(inp[0])
+    a = self.bfs(start,end)
+    print(f"Part 1: {a}")
 
-    #visited set is a tuple of move and coord
-    #ex visited set entry: (2, (1,3))
-    self.visited = set()
+    b = self.bfs(end, start)
+    c = self.bfs(start, end)
 
-    for i in range(1, len(inp)-1):
-      for j in range(1, len(inp[0])-1):
-        if inp[i][j] != ".":
-          initial_field[(i,j)].add(inp[i][j])
+    print(f"Part 2: {a+b+c}")
 
-    if DEBUG:
-      for coord, val in initial_field.items():
-        print(coord, val)
-      print()
-    self.heap = []
-    heapq.heapify(self.heap)
-    m_dist = abs(self.goal[0] - start[0]) + abs(self.goal[1] - start[1])
-    heapq.heappush(self.heap, (m_dist, 0, start, initial_field))
-    self.visited.add((0, start[0], start[1]))
-    self.tic = perf_counter()
-    while self.heap:
-      m_dist, mins, loc, field = heapq.heappop(self.heap)
-      self.bfs(mins, loc, field)
+  def bfs(self, start, goal):
+    tokens = {start}
+    steps = 0
 
-    print(f"uh: {self.res}")
-    toc = perf_counter()
-    time_diff = toc - self.tic
-    if time_diff > 60:
-      print(f"took {time_diff // 60} minutes {time_diff % 60} seconds")
-    else:
-      print(f"took {toc - self.tic:0.6f} seconds")
-    return self.res
-  
-  def bfs(self, mins, my_loc, field):
-    # prune the branch if it is impossible for us to get to the goal in less 
-    # than the current result time even if we moved optimally every minute
-    if mins >= self.res or abs(self.goal[0] - my_loc[0]) + abs(self.goal[1] - my_loc[1]) > self.res - mins:
-      return 
+    while goal not in tokens:
+      steps += 1
+      next_tokens = set()
+      for token in tokens:
+        next_tokens.add(token)
+        for dx, dy in self.dirs: #basically get every potential move & the current loc
+          nx = token[0] + dx
+          ny = token[1] + dy
+          if (0 <= ny < self.height and 0 <= nx < self.width) or (nx, ny) == goal: #add it to our next tokens if it's inbounds or our goals
+            next_tokens.add((nx,ny))
 
-    #move the blizzards
-    field = self.move_blizzards(field)
- 
-    #consider moving in each direction
-    for d_i, d_j in self.dirs:
-      new_i, new_j = my_loc[0] + d_i, my_loc[1] + d_j
-      
-      # if a candidate move is the end, update res if we 
-      # got there faster than previous branches and return
-      if self.goal == (new_i, new_j):
-        print(mins+1)
-        time_diff = perf_counter() - self.tic
-        print(f"took {time_diff // 60} minutes {time_diff % 60} seconds")
-        self.res = min(self.res, mins+1)
-        return
-      
-      # skip any attempts to move out of bounds
-      if any(
-        [new_i <= 0, new_i >= self.height-1, new_j <= 0, new_j >= self.width-1]
-      ):
-        continue 
-      
-      # don't redo work another branch has already done
-      if (mins+1, new_i, new_j) not in self.visited and (new_i, new_j) not in field:
-        self.visited.add((mins+1, new_i, new_j))
-        m_dist = abs(self.goal[0] - new_i) + abs(self.goal[1] - new_j)
-        heapq.heappush(self.heap, (m_dist, mins+1, (new_i, new_j), field))
-    
-    #wait at current location
-    if (mins+1, my_loc[0], my_loc[1]) not in self.visited and my_loc not in field:
-      self.visited.add((mins+1, my_loc[0], my_loc[1]))
-      m_dist = abs(self.goal[0] - my_loc[0]) + abs(self.goal[1] - my_loc[1])
-      heapq.heappush(self.heap, (m_dist, mins+1, my_loc, field))
-    
-  def manhattan_to_goal(self, my_loc):
-    return abs(self.goal[0] - my_loc[0]) + abs(self.goal[1] - my_loc[1]) 
-      
-  def move_blizzards(self, field):
-    updated_field = defaultdict(set)
-    for coord in field:
-      for val in field[coord]:
-        new_i, new_j = coord[0], coord[1]
-        #move blizzard based on it's symbol
-        if val == "<":
-          new_j -= 1
-        elif val == ">":
-          new_j += 1
-        elif val == "^":
-          new_i -= 1
-        elif val == "v":
-          new_i += 1
-
-        #wrap blizzards if needed
-        if new_j == 0:
-          new_j = self.width-2
-        elif new_j == self.width-1:
-          new_j = 1
-        elif new_i == 0:
-          new_i = self.height-2
-        elif new_i == self.height-1:
-          new_i = 1
-        
-        #add val to this coord in our field map
-        updated_field[(new_i, new_j)].add(val)
-    return updated_field
+      #move all winds, remove any potential moves that overlap with the wind
+      for wind in self.winds:
+        wind.move()
+        next_tokens.discard((wind.x,wind.y))
+      tokens = next_tokens
+    return steps
 
 if __name__ == "__main__":
   main()
